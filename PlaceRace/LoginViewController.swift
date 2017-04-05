a10e08d631191e2e4baa1496d8f965e35c51f865
Fauxpaw <fauxpawsc@live.com>
2017-04-05 08:51:08 -0700

    login/signup finish

--- PlaceRace/LoginViewController.swift
+++ PlaceRace/LoginViewController.swift
@@ -11,35 +11,49 @@ import ParseUI
 
 class LoginViewController: PFLogInViewController, PFSignUpViewControllerDelegate {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     func signUpViewController(_ signUpController: PFSignUpViewController, shouldBeginSignUp info: [String : String]) -> Bool {
         
-        print("Signup checking.......")
-        
         guard let name = self.signUpController?.signUpView?.usernameField?.text else {return false}
         guard let password = self.signUpController?.signUpView?.passwordField?.text else {return false}
         guard let email = self.signUpController?.signUpView?.emailField?.text else {return false}
+        guard let confirm = self.signUpController?.signUpView?.additionalField?.text else {return false}
         
         //TODO: ACTION SHEET THIS BROSEF
+        var error = false
+        var message = ""
+        
         if name.characters.count < 1 || password.characters.count < 1 || email.characters.count < 1 {
-            print("All fields must be filled in")
-            return false
+            message = "All fields must be filled in"
+            error = true
         }
         
         if name.characters.count < 3 && name.characters.count > 0   {
-            print("Invalid username, must be at least 3 letters long")
-            return false
+            message = "Invalid username, must be at least 3 letters long"
+            error = true
         }
         if password.characters.count < 7 && password.characters.count > 0 {
-            print("Invalid password, must be at least 7 characters")
-            return false
+            message = "Invalid password, must be at least 7 characters"
+            error = true
         }
         if !validateEmail(candidate: email) {
-            print("Invalid email, must contain a valid domain")
+            message = "Invalid email, must contain a valid domain"
+            error = true
+        }
+        
+        if email != confirm {
+            message = "Email fields must match"
+            error = true
+        }
+        
+        if error {
+            let controller = UIAlertController(title: "PlaceRace", message: message, preferredStyle: .alert)
+            controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.signUpController?.present(controller, animated: true, completion: nil)
+
             return false
         }
         
@@ -59,9 +73,8 @@ class LoginViewController: PFLogInViewController, PFSignUpViewControllerDelegate
     func signUpViewController(_ signUpController: PFSignUpViewController, didFailToSignUpWithError error: Error?) {
         //TODO: ACTION SHEET
         guard let error = error else { return }
-        print(error.localizedDescription)
-        
+        let controller = UIAlertController(title: "PlaceRace", message: error.localizedDescription, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.signUpController?.present(controller, animated: true, completion: nil)
     }
-    
-    
 }
