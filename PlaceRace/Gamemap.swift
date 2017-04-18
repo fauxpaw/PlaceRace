0d53b2768219d98afaa5a596003dc835967cc44d
Fauxpaw <fauxpawsc@live.com>
2017-04-17 23:05:47 -0700

    annotate play map

--- PlaceRace/Gamemap.swift
+++ PlaceRace/Gamemap.swift
@@ -100,11 +100,28 @@ extension Gamemap: CLLocationManagerDelegate {
             
             loader.getNearbyPlaces(fromEpicenter: self.lastLoc, radius: 1000, handler: { (placesDict, error) in
                 if let dict = placesDict {
-                    print(dict)
+                    //print(dict)
+                    guard let placesArray = dict.object(forKey: "results") as? [NSDictionary] else {return}
+                    for placeDictionary in placesArray {
+                        //pull out the various keys
+                        let lat = placeDictionary.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
+                        let long = placeDictionary.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
+                        //let reference = placeDictionary.object(forKey: "reference") as! String
+                        let name = placeDictionary.object(forKey: "name") as! String
+                       // let vicinity = placeDictionary.object(forKey: "vicinity") as! String
+                        //let types = placeDictionary.object(forKey: "type") as! NSArray
+                        
+                        let location = CLLocation(latitude: lat, longitude: long)
+                        let annotation = ObjectiveAnnotation(title: name, coordinate: location.coordinate)
+                        DispatchQueue.main.async {
+                            self.addAnnotation(annotation)
+                        }
+                    }
+                    
+                    
                 }
             })
         }
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
