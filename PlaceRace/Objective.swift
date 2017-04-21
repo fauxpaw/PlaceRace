0b980deb71adcea88cf2f72b8a440e39d72bebd2
Fauxpaw <fauxpawsc@live.com>
2017-04-20 18:16:37 -0700

    objective json

--- PlaceRace/Objective.swift
+++ PlaceRace/Objective.swift
@@ -10,16 +10,31 @@ import Foundation
 import Parse
 
 
-struct Objective {
+class Objective {
     
-    var name: String?
-    var imgUrl: String?
-    var info: String?
-    var category: String?
-    var coordinates: CLLocationCoordinate2D?
-    var completed = false
+    var name: String
+    //var imgUrl: String?
+    //var info: String?
+    //var category: String?
+    var coordinates: CLLocationCoordinate2D
+    var completed : Bool?
     
     /*init(name: String, imgUrl: String, info: String, category: String, coordinates: CLLocationCoordinate2D) {
     }*/
     
+    init? (json: [String: Any]) {
+    
+        guard let geoDict = json["geometry"] as? [String: Any]  else {print("geoDict not found");return nil }
+        guard let locDict = geoDict["location"] as? [String: Any] else {print("locDict not found");return nil}
+    
+        if let locName = json["name"] as? String, let latitude = locDict["lat"] as? CLLocationDegrees, let longitude = locDict["lng"] as? CLLocationDegrees {
+            let loc = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            self.coordinates = loc
+            self.completed = false
+            self.name = locName
+        } else {
+            print("could not instantiate objective. An 'if let' failed")
+            return nil
+        }
+    }
 }
