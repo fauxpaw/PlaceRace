6cafac22572339f0e68500270531c99c1e9cd9d5
Fauxpaw <fauxpawsc@live.com>
2017-04-20 22:54:57 -0700

    game options cell height adjust

---
+++ PlaceRace/ParseServerAPI.swift
@@ -0,0 +1,19 @@
+//
+//  ParseServerAPI.swift
+//  PlaceRace
+//
+//  Created by Michael Sweeney on 4/20/17.
+//  Copyright © 2017 Michael Sweeney. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class ParseServerAPI  {
+    
+    //CREATE
+    //READ
+    //UPDATE
+    //DELETE
+    
+}
