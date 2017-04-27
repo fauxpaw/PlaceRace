5b24272300240f753be7b40cb6b11915b2c76219
Fauxpaw <fauxpawsc@live.com>
2017-04-26 22:31:24 -0700

    add combination measure test

--- PlaceRaceTests/ShuffleTest.swift
+++ PlaceRaceTests/ShuffleTest.swift
@@ -47,29 +47,33 @@ class ShuffleTest: XCTestCase {
     
     func testPossibleCombinations() {
         
+        let array = [1,2,3,4,5]
+        var count = 0
+        
+        func permuteWirth<T>(_ a: [T], _ n: Int) {
+            if n == 0 {
+                //print(a) // display the current permutation
+                count += 1
+            } else {
+                var a = a
+                permuteWirth(a, n - 1)
+                for i in 0..<n {
+                    swap(&a[i], &a[n])
+                    permuteWirth(a, n - 1)
+                    swap(&a[i], &a[n])
+                }
+            }
+        }
+        
+        permuteWirth(array, array.count - 1)
+        XCTAssertEqual(count, 120)
         
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            let array = [1,2,3,4,5,6,7]
-            
-            func permuteWirth<T>(_ a: [T], _ n: Int) {
-                if n == 0 {
-                    print(a)   // display the current permutation
-                } else {
-                    var a = a
-                    permuteWirth(a, n - 1)
-                    for i in 0..<n {
-                        swap(&a[i], &a[n])
-                        permuteWirth(a, n - 1)
-                        swap(&a[i], &a[n])
-                    }
-                }
-            }
-            
-            permuteWirth(array, array.count - 1)
+            self.testPossibleCombinations()
         }
     }
     
